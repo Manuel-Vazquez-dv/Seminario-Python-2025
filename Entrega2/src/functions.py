d03705c5e8b8d35de8c94dee73e9a32fa2dcc47e
ZidenDv <manuel.vazquez.2003@gmail.com>
2025-04-03 19:34:50 -0300

    Se modifico el diccionario de puntos totales para tener en cuenta si los nombres no son los preestablecidos o hay jugadores nuevos. Se agregaron comentarios en varias lineas para mejorar la claridad del codigo.

--- Entrega2/src/functions.py
+++ Entrega2/src/functions.py
@@ -11,16 +11,18 @@ def sortVocal(toSort):
 
 #Ejercicio 10
 
-def display_round(current, actualround):
+def display_round(current):
       """
       Imprime en consola las estadisticas totales de los jugadores utilizando los valores enviados.
 
       :param current: Diccionario con los valores totales de cada jugador.
       """
+      #Se ordena el diccionario 'current' (el diccionario de totales) por el valor de 'points' de cada jugador.
       sortedCurrent = sorted(current,key=lambda x: current[x]['points'], reverse=True)
-      print(f"Ronda {actualround}")
+      #Se imprime el encabezado de la tabla.
       print(f"Nombre   Kills   Asistencias   Muertes   MVP's   Puntos totales\n")
       print("-------------------------------------------------------------------")
+      #Se imprime el nombre de cada jugador y sus estadisticas totales.
       for elem in sortedCurrent:
             print(f"{elem:<8}{current[elem]['kills']:>5}{current[elem]['assists']:>12}{current[elem]['deaths']:>10}{current[elem]['MVPS']:>8}{current[elem]['points']:>15}")
       print("-------------------------------------------------------------------")      
@@ -34,20 +36,30 @@ def process_round(currentRound, total, actualround):
       :param total: Diccionario con las estadisticas de los jugadores, teniendo en cuenta todas las rondas previas.
       :param actualround: valor int de la ronda actual, utilizado para "display_round", como contador de ronda.
      """
-     currLeader = "dummyvalue"
+     currLeader = "dummyvalue - Si se imprime esto se debe a un error."
      currLPoints = -99999
      for elem in currentRound:
+           #Se actualizan las estadisticas totales de kills y assists.
            total[elem]['kills'] += currentRound[elem]['kills'] 
            total[elem]['assists'] += currentRound[elem]['assists']
+           #Se cuentan los puntos de la ronda actual y se almacenan en 'actualPoints'.
            actualPoints = (currentRound[elem]['kills']*3 + currentRound[elem]['assists'])
+           #Si el jugador ha muerto, se le resta un punto y se le suma uno al total de muertes.
            if (currentRound[elem]['deaths']): 
                 total[elem]['deaths'] += 1
                 actualPoints -= 1  
+           #Se actualizan las estadisticas totales de puntos.     
            total[elem]['points'] += actualPoints     
+           #Se determina si el jugador es el MVP de la ronda, comparando los puntos actuales con los puntos del MVP actual.
            if actualPoints>currLPoints:
                  currLeader=elem
                  currLPoints=actualPoints           
-     total[currLeader]['MVPS'] += 1            
-     display_round(total, actualround)
+     #Se utiliza la variable 'currLeader' para actualizar el total de MVP's del jugador correspondiente.            
+     total[currLeader]['MVPS'] += 1
+     #Se imprime el numero de la ronda y el nombre del MVP de la ronda.
+     print(f"Ronda {actualround}")
+     print(f"MVP de la ronda: {currLeader}")            
+     display_round(total)
+     #Se suma uno al contador de ronda, para que la siguiente vez que se imprima el numero de ronda correcto.
      actualround+=1    
      return actualround  
